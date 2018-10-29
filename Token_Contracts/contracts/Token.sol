9cfe31840b5e38db283125b8f3a211bd71acc0ba
chris <chrisjfranko@gmail.com>
2018-10-29 11:16:35 -0400

    update files

--- Token_Contracts/contracts/Token.sol
+++ Token_Contracts/contracts/Token.sol
@@ -1,48 +1,69 @@
-// Abstract contract for the full ERC 20 Token standard
-// https://github.com/ethereum/EIPs/issues/20
-pragma solidity ^0.4.8;
+/*
+Implements modular EIP20 token standard ERC644: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+.*/
 
-contract Token {
-    /* This is a slight change to the ERC20 base standard.
-    function totalSupply() constant returns (uint256 supply);
-    is replaced with:
-    uint256 public totalSupply;
-    This automatically creates a getter function for the totalSupply.
-    This is moved to the base contract since public getter functions are not
-    currently recognised as an implementation of the matching abstract
-    function by the compiler.
-    */
-    /// total amount of tokens
-    uint256 public totalSupply;
 
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
-    function balanceOf(address _owner) constant returns (uint256 balance);
+pragma solidity ^0.4.21;
 
-    /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success);
+import"./Balances.sol";
 
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+contract ERC644 {
 
-    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @param _value The amount of tokens to be approved for transfer
-    /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) returns (bool success);
-
-    /// @param _owner The address of the account owning tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-
+    // solhint-disable-next-line no-simple-event-func-name
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    uint256 constant private MAX_UINT256 = 2**256 - 1;
+
+    Balances public balances;
+
+    string public name;                   //fancy name: eg Franko Bucks
+    uint8 public decimals;                //How many decimals to show.
+    string public symbol;                 //An identifier: eg FRK
+
+    constructor(address _balances, string _name, string _symbol, uint8 _dec) public {
+        balances = Balances(_balances);
+        name = _name;                                   // Set the name for display purposes
+        decimals = _dec;                            // Amount of decimals for display purposes
+        symbol = _symbol;                               // Set the symbol for display purposes
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balances.getBalance(msg.sender) >= _value);
+        balances.decBalance(msg.sender, _value);
+        balances.incBalance(_to, _value);
+        emit Transfer(msg.sender, _to, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        uint256 allowance = balances.getAllowance(_from, msg.sender);
+        require(balances.getBalance(_from) >= _value && allowance >= _value);
+        balances.incBalance(_to, _value);
+        balances.decBalance(_from, _value);
+        if (allowance < MAX_UINT256) {
+            balances.decApprove(_from, msg.sender, _value);
+        }
+        emit Transfer(_from, _to, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances.getBalance(_owner);
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+      //allowed[msg.sender][_spender] = _value;
+      balances.setApprove(msg.sender, _spender, _value);
+      emit Approval(msg.sender, _spender, _value);
+      return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+      return balances.getAllowance(_owner, _spender);
+    }
+
+    function totalSupply() public view returns(uint256){
+      return balances.getTotalSupply();
+    }
 }
